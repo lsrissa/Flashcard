e1a4af91815d2bbfe88afc81f383a9733ce0fd23
lsrissa <00001125073731sp@al.educacao.sp.gov.br>
2024-12-02 08:22:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Lingua inglesa',
+    'como se diz azul em inglês',
+    'Azul em inglês é BLUE (bluei)'
+)
+/* Larissa.Georgiane.2c.26/11/2024
+*/
+criaCartao(
+    'Ginastica artistica',
+    'O que são paralelas?',
+    'È um aparelho utilizado pelos homens na ginastica.'
+)
+
+criaCartao(
+    'Coreia do sul',
+      'Qual é a comida mais famosa da coreia do sul',
+    'A comida mais famosa da coreia do sul é o (KIMCHI).'
+)
+
+criaCartao(
+    'Pericia criminal',
+    'O que é pericia criminal?',
+    'Atividade  tecnico-cientifica que analisa vestigios de crime.'
+)
